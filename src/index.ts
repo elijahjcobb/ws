5b2240512dc096fb34b69547bbe87c7030e7fdb4
Elijah Cobb <elijah@elijahcobb.com>
2019-08-08 16:25:33 -0400

    initial

--- src/index.ts
+++ src/index.ts
@@ -102,6 +102,9 @@ export interface ECWSIError {
 	code: number;
 }
 
+export type PromiseResolve<T> = (value?: T | PromiseLike<T> | undefined) => void;
+export type PromiseReject = (reason?: any) => void;
+
 export interface ECWSIMessageResponse<P = object> {
 	request: ECWSRequest;
 	response: {
@@ -143,9 +146,9 @@ export class ECWSCommand {
 
 	public readonly cmd: string;
 	public readonly typeValidator: ObjectType;
-	public readonly handler: (req: ECWSRequest) => Promise<ECWSResponse>;
+	public readonly handler: (req: ECWSRequest, connection: ECWSConnection) => Promise<ECWSResponse>;
 
-	public constructor(cmd: string, types: ObjectTypeDefinition, handler: (req: ECWSRequest) => Promise<ECWSResponse>) {
+	public constructor(cmd: string, types: ObjectTypeDefinition, handler: (req: ECWSRequest, connection: ECWSConnection) => Promise<ECWSResponse>) {
 
 		this.cmd = cmd;
 		this.typeValidator = new ObjectType(types);
@@ -155,71 +158,34 @@ export class ECWSCommand {
 
 }
 
-export abstract class ECWSManager<T extends (WebSocket | WebSocket.Server)> {
-
-	public commands: Dictionary<string, ECWSCommand>;
-	public openRequests: ArrayList<string>;
-	public socket: T;
-
-	protected constructor(socket: T) {
-
-		this.commands = new Dictionary<string, ECWSCommand>();
-		this.openRequests = new ArrayList<string>();
-		this.socket = socket;
-
-	}
-
-	public async parseRequest(data: Buffer): Promise<ECWSRequest> {
-
-		const messageDecoded: string = data.toString("utf8");
-
-		let messageObject: ECWSIMessage;
-
-		try {
-			messageObject = JSON.parse(messageDecoded);
-		} catch (e) {
-			throw ECWSError.init().msg("The payload was not valid JSON.").code(400).passthrough();
-		}
-
-		let objectTypeValidator: ObjectType = new ObjectType({
-			cmd: StandardType.STRING,
-			meta: new ObjectType(),
-			payload: new ObjectType()
-		});
-
-		const isMessageValid: boolean = objectTypeValidator.checkConformity(messageObject);
-		if (!isMessageValid) throw ECWSError.init().msg("Message doesn't have valid types.").code(400).passthrough();
-
-		const command: ECWSCommand | undefined = this.commands.get(messageObject.cmd);
-		if (!command) throw ECWSError.init().msg("Command does not exist.").code(404).passthrough();
-
-		const payload: object = messageObject.payload;
-		const isPayloadValid: boolean = command.typeValidator.checkConformity(payload);
-		if (!isPayloadValid) throw ECWSError.init().msg("Message payload doesn't have valid types.").code(400).passthrough();
-
-		return new ECWSRequest(messageObject, command);
-
-	}
-
-}
-
 export class ECWSConnection {
 
-	protected readonly socket: WebSocket;
 	protected commands: Dictionary<string, ECWSCommand>;
-	protected openRequests: Set<string>;
+	protected openRequests: Dictionary<string, (res: ECWSIMessageResponse) => void>;
 
+	public readonly socket: WebSocket;
 	public readonly id: string;
+	public readonly ip: string;
 	public onOpenHandler: (() => void) | undefined;
 	public onCloseHandler: (() => void) | undefined;
 	public onMessageHandler: (() => void) | undefined;
 
-	public constructor(ws: WebSocket, id: string, commands: Dictionary<string, ECWSCommand>) {
+	public constructor(ws: WebSocket, id: string, ip: string, commands: Dictionary<string, ECWSCommand>) {
 
+		this.ip = ip;
 		this.socket = ws;
 		this.id = id;
 		this.commands = commands;
-		this.openRequests = new Set<string>();
+		this.openRequests = new Dictionary<string, (res: ECWSIMessageResponse) => void>();
+
+	}
+
+	private generateNewRequestId(): string {
+
+		let id: string = ECGenerator.randomId();
+		while (this.openRequests.hasKey(id)) id = ECGenerator.randomId();
+
+		return id;
 
 	}
 
@@ -256,20 +222,21 @@ export class ECWSConnection {
 
 	private async handleMessageReceived(data: Buffer): Promise<void> {
 
-		const req: ECWSRequest = await this.parseRequest(data);
+		const req: ECWSRequest | undefined = await this.parseRequest(data);
+		if (!req) return;
 		const cmd: ECWSCommand | undefined = this.commands.get(req.cmd);
 		if (!cmd) throw ECWSError.init().msg("Command does not exist.").code(404).passthrough();
 
-		const res: ECWSResponse = await cmd.handler(req);
+		const res: ECWSResponse = await cmd.handler(req, this);
 		await this.sendResponse(res);
 
 	}
 
-	private async parseRequest(data: Buffer): Promise<ECWSRequest> {
+	private async parseRequest(data: Buffer): Promise<ECWSRequest | undefined> {
 
 		const messageDecoded: string = data.toString("utf8");
 
-		let messageObject: ECWSIMessage;
+		let messageObject: ECWSIMessage | ECWSIMessageResponse;
 
 		try {
 			messageObject = JSON.parse(messageDecoded);
@@ -277,23 +244,44 @@ export class ECWSConnection {
 			throw ECWSError.init().msg("The payload was not valid JSON.").code(400).passthrough();
 		}
 
-		let objectTypeValidator: ObjectType = new ObjectType({
-			cmd: StandardType.STRING,
-			meta: new ObjectType(),
-			payload: new ObjectType()
-		});
+		if (messageObject.hasOwnProperty("request") && messageObject.hasOwnProperty("response")) {
 
-		const isMessageValid: boolean = objectTypeValidator.checkConformity(messageObject);
-		if (!isMessageValid) throw ECWSError.init().msg("Message doesn't have valid types.").code(400).passthrough();
+			const body: ECWSIMessageResponse = messageObject as ECWSIMessageResponse;
+			const id: string = body.request.meta.id;
 
-		const command: ECWSCommand | undefined = this.commands.get(messageObject.cmd);
-		if (!command) throw ECWSError.init().msg("Command does not exist.").code(404).passthrough();
+			const handler: ((res: ECWSIMessageResponse) => void) | undefined = this.openRequests.get(id);
+			if (handler) {
 
-		const payload: object = messageObject.payload;
-		const isPayloadValid: boolean = command.typeValidator.checkConformity(payload);
-		if (!isPayloadValid) throw ECWSError.init().msg("Message payload doesn't have valid types.").code(400).passthrough();
+				handler(body);
+				this.openRequests.remove(id);
 
-		return new ECWSRequest(messageObject, command);
+				return;
+
+			}
+
+		} else {
+
+			const body: ECWSIMessage = messageObject as ECWSIMessage;
+
+			let objectTypeValidator: ObjectType = new ObjectType({
+				cmd: StandardType.STRING,
+				meta: new ObjectType(),
+				payload: new ObjectType()
+			});
+
+			const isMessageValid: boolean = objectTypeValidator.checkConformity(messageObject);
+			if (!isMessageValid) throw ECWSError.init().msg("Message doesn't have valid types.").code(400).passthrough();
+
+			const command: ECWSCommand | undefined = this.commands.get(body.cmd);
+			if (!command) throw ECWSError.init().msg("Command does not exist.").code(404).passthrough();
+
+			const payload: object = body.payload;
+			const isPayloadValid: boolean = command.typeValidator.checkConformity(payload);
+			if (!isPayloadValid) throw ECWSError.init().msg("Message payload doesn't have valid types.").code(400).passthrough();
+
+			return new ECWSRequest(body, command);
+
+		}
 
 	}
 
@@ -325,9 +313,9 @@ export class ECWSConnection {
 
 	}
 
-	public async call(cmd: string, payload: object): Promise<ECWSResponse> {
+	public callAsync(cmd: string, payload: object, handler: (res: ECWSIMessageResponse) => void): void {
 
-		const id: string = "";
+		const id: string = this.generateNewRequestId();
 		const req: ECWSIMessage<object> = {
 			cmd,
 			meta: {
@@ -350,14 +338,20 @@ export class ECWSConnection {
 		}
 
 		this.socket.send(data);
-		this.openRequests.add(id);
-
-		// wait and routinely check for request to be responded to in on message
-		// configure on message to be a response to a request on everything
-
-		return new ECWSResponse({});
+		this.openRequests.set(id, handler);
 
 	}
+
+	public call(cmd: string, payload: object): Promise<ECWSIMessageResponse> {
+
+		return new Promise<ECWSIMessageResponse>(((resolve: PromiseResolve<ECWSIMessageResponse>): void => {
+
+			this.callAsync(cmd, payload, resolve);
+
+		}));
+
+	}
+
 
 }
 
@@ -365,7 +359,7 @@ export class ECWSSocket extends ECWSConnection implements ECWSCommandable {
 
 	public constructor(url: string) {
 
-		super(new WebSocket(url), "", new Dictionary<string, ECWSCommand>());
+		super(new WebSocket(url), "", "1.1.1.1", new Dictionary<string, ECWSCommand>());
 
 	}
 
@@ -384,12 +378,23 @@ export class ECWSServer implements ECWSCommandable {
 	private readonly options: WebSocket.ServerOptions | undefined;
 	private authorizationHandler: ((req: HTTP.IncomingMessage) => Promise<void>) | undefined;
 	private readonly commands: Dictionary<string, ECWSCommand>;
+	protected openRequests: Dictionary<string, (res: ECWSIMessageResponse) => void>;
 
 	public constructor(options?: WebSocket.ServerOptions) {
 
 		this.connections = new Dictionary<string, ECWSConnection>();
 		this.commands = new Dictionary<string, ECWSCommand>();
 		this.options = options;
+		this.openRequests = new Dictionary<string, (res: ECWSIMessageResponse) => void>();
+
+	}
+
+	private generateNewRequestId(): string {
+
+		let id: string = ECGenerator.randomId();
+		while (this.openRequests.hasKey(id)) id = ECGenerator.randomId();
+
+		return id;
 
 	}
 
@@ -402,7 +407,7 @@ export class ECWSServer implements ECWSCommandable {
 
 	}
 
-	public register(cmd: string, types: ObjectTypeDefinition, handler: (req: ECWSRequest) => Promise<ECWSResponse>): void {
+	public register(cmd: string, types: ObjectTypeDefinition, handler: (req: ECWSRequest, connection: ECWSConnection) => Promise<ECWSResponse>): void {
 
 		this.commands.set(cmd, new ECWSCommand(cmd, types, handler));
 
@@ -420,18 +425,55 @@ export class ECWSServer implements ECWSCommandable {
 
 			}); }
 
-			const connection: ECWSConnection = new ECWSConnection(socket, this.generateNewConnectionId(), this.commands);
+			socket.on
+
+			const connection: ECWSConnection = new ECWSConnection(socket, this.generateNewConnectionId(), request.connection.remoteAddress || request.connection.localAddress, this.commands);
 			this.connections.set(connection.id, connection);
 
 		});
 
 	}
 
+	public callAsync(connectionId: string, cmd: string, payload: object, handler: (res: ECWSIMessageResponse) => void): void {
+
+		const id: string = this.generateNewRequestId();
+		const req: ECWSIMessage<object> = {
+			cmd,
+			meta: {
+				id,
+				timestamp: Date.now()
+			},
+			payload
+		};
+
+		let data: Buffer;
+
+		try {
+
+			data = Buffer.from(JSON.stringify(req), "utf8");
+
+		} catch (e) {
+
+			throw ECWSError.init().msg("Could not encode payload.").passthrough();
+
+		}
+
+		const connection: ECWSConnection | undefined = this.connections.get(connectionId);
+		if (connection) connection.socket.send(data);
+		this.openRequests.set(id, handler);
+
+	}
+
 }
+
+
+
 
 const server: ECWSServer = new ECWSServer({ port: 8080 });
 
-server.register("hi", {foo: StandardType.STRING}, async(req: ECWSRequest): Promise<ECWSResponse> => {
+server.register("hi", {foo: StandardType.STRING}, async(req: ECWSRequest, connection: ECWSConnection): Promise<ECWSResponse> => {
+
+	console.log(connection.ip);
 
 	return new ECWSResponse({bar: req.payload});
 
@@ -448,8 +490,8 @@ socket.register("hi", {}, async(req: ECWSRequest): Promise<ECWSResponse> => {
 
 });
 
-socket.call("hi", {foo: "Hello, world!"}).then((res: ECWSResponse) => {
+socket.call("hi", {foo: "Hello, world!"}).then((res: ECWSIMessageResponse) => {
 
-	console.log(res.payload);
+	console.log(res);
 
 }).catch((err: any) => {});
